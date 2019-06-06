f888c2c58db48fdfabed79d4a28cef382951bd9d
Aviv Bueno <aviv.buen@gmail.com>
2019-06-07 00:30:33 +0300

    Connecting webpack with CI

--- scripts/deploy-dev-to-s3.sh
+++ scripts/deploy-dev-to-s3.sh
@@ -2,7 +2,7 @@
 
 # ============================  Script Variables   ============================
 DEPLOY_BUCKET="bartdev.gariany.com"
-PUBLIC_FOLDER="../public"
+PUBLIC_FOLDER="../dist"
 PUBLIC_URL="http://bartdev.gariany.com"
 # =============================================================================
 
