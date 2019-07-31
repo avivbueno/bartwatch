2a0809a1c7412abbe93074cbb0f3542421a3f17d
Aviv Bueno <aviv.buen@gmail.com>
2019-07-31 21:19:15 +0300

    Adding material-ui and Navbar

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -3,27 +3,32 @@ import TrainMap from "./containers/TrainMap.jsx";
 import { connect } from "react-redux";
 import { setMapSize, setMapType } from "./actions";
 import { mapType } from "./enums";
+import Navbar from "./ui/Navbar.jsx";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    //Just as an example for map types
+    this.props.dispatch(setMapType(mapType.satellite));
   }
   resize() {
-    this.props.dispatch(setMapSize({ height: window.innerHeight - 10 }));
+    this.props.dispatch(setMapSize({ height: window.innerHeight - 74 }));
   }
   componentDidMount() {
     this.resize();
     window.addEventListener("resize", this.resize.bind(this));
-
-    //Just as an example for map types
-    this.props.dispatch(setMapType(mapType.satellite));
   }
   componentWillUnmount() {
     window.removeEventListener("resize", this.resize.bind(this));
   }
   render() {
-    return <TrainMap />;
+    return (
+      <div>
+        <Navbar />
+        <TrainMap />
+      </div>
+    );
   }
 }
 
