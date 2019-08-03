7d951345afe422d4f550197782af39b04a25f0f5
Aviv Bueno <aviv.buen@gmail.com>
2019-08-03 23:18:24 +0300

    Revert "Converting map to a component (without trains and stations)"

--- src/js/containers/TrainMap.jsx
+++
@@ -1,8 +0,0 @@
-import { connect } from "react-redux";
-import Map from "../components/Map.jsx";
-
-const mapStateToProps = state => ({
-  map: state.map
-});
-
-export default connect(mapStateToProps)(Map);
--- src/js/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import liveTrains from "./liveTrains.js";
-import map from "./map.js";
-
-export default combineReducers({
-  liveTrains,
-  map
-});
